c73f254b2ce3d036475072f50a968c278a05c83d
marlennr <marlennr@users.noreply.github.com>
2026-04-06 00:08:55 +0000

    assignment 5

--- src/OSDev22/src/kernel.c
+++ src/OSDev22/src/kernel.c
@@ -10,6 +10,7 @@
 #include "keyboard.h"
 #include "memory.h"
 #include "pit.h"
+#include "song/song.h"
 
 extern uint32_t end;
 
@@ -19,37 +20,43 @@ struct multiboot_info {
     struct multiboot_tag *first;
 };
 
+/* Two simple songs */
+static Note simple_song_1[] = {
+    {C4, 300}, {D4, 300}, {E4, 300}, {F4, 300},
+    {G4, 300}, {R, 150},
+    {G4, 300}, {F4, 300}, {E4, 300}, {D4, 300},
+    {C4, 500}
+};
+
+static Note simple_song_2[] = {
+    {E4, 250}, {G4, 250}, {A4, 400}, {R, 150},
+    {A4, 250}, {G4, 250}, {E4, 400}, {R, 150},
+    {D4, 250}, {F4, 250}, {G4, 400}, {R, 150},
+    {C4, 500}
+};
+
 int main(uint32_t magic, struct multiboot_info* mb_info_addr) {
     (void)magic;
     (void)mb_info_addr;
 
-    /* Sett opp GDT - må gjøres først for riktig segmentering */
     gdt_init();
-
-    /* Klargjør skjermen */
     terminal_initialize();
 
-    /* Sett opp interrupt-systemet */
-    idt_init();     /* Opprett tom IDT-tabell */
-    isr_init();     /* Registrer CPU-exceptions (0-31) */
-    irq_init();     /* Remap PIC + registrer hardware IRQ-er (32-47) + sti */
+    idt_init();
+    isr_init();
+    irq_init();
 
-    /* Initialiser minnehåndtering */
     init_kernel_memory(&end);
     init_paging();
     print_memory_layout();
 
-    /* Initialiser PIT */
     init_pit();
-
-    /* Start tastatur-driver */
     keyboard_init();
 
     printf("Hello World\n");
     printf("Interrupt-system aktivert!\n");
-    printf("Write something on keyboard:\n");
+    printf("Music player starting...\n");
 
-    /* Test malloc */
     void* some_memory = malloc(12345);
     void* memory2 = malloc(54321);
     void* memory3 = malloc(13331);
@@ -59,16 +66,21 @@ int main(uint32_t magic, struct multiboot_info* mb_info_addr) {
     printf("memory2     = 0x%x\n", (uint32_t)memory2);
     printf("memory3     = 0x%x\n", (uint32_t)memory3);
 
-    /* Test PIT */
-    uint32_t counter = 0;
-    while (true) {
-        printf("[%d]: Sleeping with busy-waiting (HIGH CPU).\n", counter);
-        sleep_busy(1000);
-        printf("[%d]: Slept using busy-waiting.\n", counter++);
+    Song songs[] = {
+        {simple_song_1, sizeof(simple_song_1) / sizeof(Note)},
+        {simple_song_2, sizeof(simple_song_2) / sizeof(Note)}
+    };
 
-        printf("[%d]: Sleeping with interrupts (LOW CPU).\n", counter);
-        sleep_interrupt(1000);
-        printf("[%d]: Slept using interrupts.\n", counter++);
+    uint32_t n_songs = sizeof(songs) / sizeof(Song);
+    SongPlayer* player = create_song_player();
+
+    while (true) {
+        for (uint32_t i = 0; i < n_songs; i++) {
+            printf("Playing song %d...\n", i + 1);
+            player->play_song(&songs[i]);
+            printf("Finished song %d.\n", i + 1);
+            sleep_interrupt(500);
+        }
     }
 
     return 0;
