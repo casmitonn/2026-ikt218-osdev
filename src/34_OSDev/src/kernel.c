b9b835577efb5a2dbf38a2df4e086f4d3a7471aa
Karen <karen@bjorbekk.no>
2026-04-11 14:19:54 +0000

    Assignmnet 3: Interrupts

--- src/34_OSDev/src/kernel.c
+++ src/34_OSDev/src/kernel.c
@@ -1,5 +1,7 @@
 #include "terminal.h"
 #include "gdt.h"
+#include "idt.h"
+#include "irq.h"
 
 void main(uint32_t mb_magic, void* mb_info) {
     (void)mb_magic; //stop warnings about unused variables
@@ -7,6 +9,12 @@ void main(uint32_t mb_magic, void* mb_info) {
 
     terminal_initialize();
     init_gdt();
+    init_idt();
+    init_irq();
+
+    __asm__ volatile ("sti"); //enable interrupts after PIC is remapped
+
+    //asm("int $0x1"); //manually trigger interrupt
 
     printf("Hello World\n");
 
